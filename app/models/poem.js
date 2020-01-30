9c010fcaa3ee016a182ec5c9742a985f8ce453d0
RWPicklechips <hminicucci@gmail.com>
2020-01-30 13:53:54 -0500

    API owner fix
    - added owner id and user is values

--- app/models/poem.js
+++ app/models/poem.js
@@ -16,6 +16,11 @@ const poemSchema = new mongoose.Schema({
   year: {
     type: Number,
     required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
   timestamps: true
--- app/routes/poem_routes.js
+++ app/routes/poem_routes.js
@@ -9,11 +9,13 @@ const router = express.Router()
 const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
 const removeBlanks = require('../../lib/remove_blank_fields')
+const requireOwnership = customErrors.requireOwnership
 
 // INDEX
 // GET /poems
 router.get('/poems', requireToken, (req, res, next) => {
   Poem.find()
+    .populate('owner', 'email')
     .then(poems => {
       // uses .map to convert 'poems' to POJO to use .toObject
       return poems.map(poem => poem.toObject())
@@ -28,6 +30,7 @@ router.get('/poems/:id', requireToken, (req, res, next) => {
   // req.params.id will is set to the `:id` passed
   Poem.findById(req.params.id)
     // custom error handler for 404
+    .populate('owner')
     .then(handle404)
     .then(poem => res.status(200).json({ poem: poem.toObject() }))
     .catch(next)
@@ -36,6 +39,7 @@ router.get('/poems/:id', requireToken, (req, res, next) => {
 // CREATE
 // POST /poems
 router.post('/poems', requireToken, (req, res, next) => {
+  req.body.poem.owner = req.user.id
   Poem.create(req.body.poem)
     .then(poem => {
       res.status(201).json({ poem: poem.toObject() })
@@ -46,9 +50,11 @@ router.post('/poems', requireToken, (req, res, next) => {
 // UPDATE
 // PATCH /poems/<ID>
 router.patch('/poems/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.example.owner
   Poem.findById(req.params.id)
     .then(handle404)
     .then(poem => {
+      requireOwnership(req, poem)
       return poem.updateOne(req.body.poem)
     })
     .then(() => res.sendStatus(204))
